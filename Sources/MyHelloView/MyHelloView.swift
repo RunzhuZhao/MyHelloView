9b0626f7bce22991810a4bab35e93900dd08f598
Run <runzhuzhao@gmail.com>
2025-07-28 10:43:24 +0800

    init set up

---
+++ Sources/MyHelloView/MyHelloView.swift
@@ -0,0 +1,23 @@
+//
+//  MyHelloView.swift
+//
+//
+//  Created by Run on 2025/7/28.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct MyHelloView: View {
+    public var body: some View {
+        Text("Hello, 慕课网!")
+    }
+    public init() {
+        
+    }
+}
+
+@available(iOS 13.0, *)
+#Preview {
+    MyHelloView()
+}
